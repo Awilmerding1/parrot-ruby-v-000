e255099412e4e4f7ca620ead84e2da88fe84a6e9
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-06 17:58:35 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string = "Squawk!")
-  if string
-  puts "Pretty bird!"
-  return "Pretty bird!"
-end
+  return string
 end
